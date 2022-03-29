b42a0375c70bcab7a6e2ecdcb42f20eb337d008a
Sébastien Cardon <s.cardon@outlook.com>
2022-03-29 12:01:33 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: 'jit',
-  content: ['./*.html', './assets/js/*.js],
+  content: ['./*.html', './assets/js/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
